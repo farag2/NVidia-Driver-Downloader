ee447c9d773e404ce61b599798e63d58b6d028c7
Dmitry Nefedov <nefedovds@ya.ru>
2022-09-29 13:52:35 +0300

    Update Download_NVidia_Driver.ps1

--- Download_NVidia_Driver.ps1
+++ Download_NVidia_Driver.ps1
@@ -44,13 +44,14 @@ function UpdateNVidiaDriver
 	{
 		# The NVIDIA System Management Interface (nvidia-smi) is a command line utility, based on top of the NVIDIA Management Library (NVML)
 		$CurrentDriverVersion = nvidia-smi.exe --format=csv,noheader --query-gpu=driver_version
-		Write-Verbose -Message "Current version: $CurrentDriverVersion" -Verbose
 	}
-	else
+	elseif
 	{
-		Write-Warning -Message "Cannot find nvidia-smi.exe required file"
-		exit
+		[System.Version]$Driver = (Get-CimInstance -ClassName Win32_VideoController | Where-Object -FilterScript {$_.Name -match "NVIDIA"}).DriverVersion
+		$CurrentDriverVersion = ("{0}{1}" -f $Driver.Build, $Driver.Revision).Substring(1).Insert(3,'.')
 	}
+
+	Write-Verbose -Message "Current version: $CurrentDriverVersion" -Verbose
 
 	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
